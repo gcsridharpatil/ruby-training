1d8810b48058182c47babaad870efc88d2611325
adminn <sridhar.patil@globacap.com>
2021-12-10 22:37:21 +0530

    Removed redundant code and refactored the code

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -1,119 +1,54 @@
 # frozen_string_literal: true
 
 class Bottles
-    def song
-        verses(99, 0)
-        sing
+  def song
+    verses(99, 0)
+    sing
+  end
 
-    end
+  def sing
+    verses(99, 0)
+  end
 
-    def sing
-      verses(99, 0)
-    end
+  # This is a loop that will repeat anything within the braces (in this case, verse(number)) a hundred times, with number going from 99 to 0.
+  # Iterates the block, Passing decresing values
+  def verses(starting, ending)
+    starting.downto(ending).map { |i| verse(i) }.join("\n")
+  end
 
-    def verses(starting, ending)
-    # starting.downto(ending).map {verse(number)}.join("\n")
-    starting.downto(ending).map {|i| verse(i)}.join("\n")
-    end
+  def verse(number)
+    case number
+    when 6
 
-    def verse(number)
-        case number
-      #   when 8
+      '6 bottles of beer on the wall, ' \
+      "6 bottles of beer.\n" \
+      'Take one down and pass it around, ' \
+      "5 bottles of beer on the wall.\n\n"
 
-      #   "8 bottles of beer on the wall, " +
-      #   "8 bottles of beer.\n" +
-      #   "Take one down and pass it around, " +
-      #   "7 bottles of beer on the wall.\n"
+    when 1
 
-      # when 7
+      '1 bottle of beer on the wall, ' \
+      "1 bottle of beer.\n" \
+      'Take it down and pass it around, ' \
+      "no more bottles of beer on the wall.\n"
 
-      #   "7 bottles of beer on the wall, " +
-      #   "7 bottles of beer.\n" +
-      #   "Take one down and pass it around, " +
-      #   "6 bottles of beer on the wall.\n"
+    when 2
 
-      when 6
+      '2 bottles of beer on the wall, ' \
+      "2 bottles of beer.\n" \
+      'Take one down and pass it around, ' \
+      "1 bottle of beer on the wall.\n"
 
-        "6 bottles of beer on the wall, " +
-        "6 bottles of beer.\n" +
-        "Take one down and pass it around, " +
-        "5 bottles of beer on the wall.\n\n"
+    when 0
 
-        # when 3
+      'No more bottles of beer on the wall, ' \
+      "no more bottles of beer.\n" \
+      'Go to the store and buy some more, ' \
+      "99 bottles of beer on the wall.\n"
 
-        # "3 bottles of beer on the wall, " +
-        # "3 bottles of beer.\n" +
-        # "Take one down and pass it around, " +
-        # "2 bottles of beer on the wall.\n"
+    else
+      "#{number} bottles of beer on the wall, #{number} bottles of beer.\nTake one down and pass it around, #{number - 1} bottles of beer on the wall.\n"
 
-        when 1
-
-        "1 bottle of beer on the wall, " +
-        "1 bottle of beer.\n" +
-        "Take it down and pass it around, " +
-        "no more bottles of beer on the wall.\n"
-
-      when 2
-
-        "2 bottles of beer on the wall, " +
-        "2 bottles of beer.\n" +
-        "Take one down and pass it around, " +
-        "1 bottle of beer on the wall.\n"
-
-      when 0
-
-        "No more bottles of beer on the wall, " +
-        "no more bottles of beer.\n" +
-        "Go to the store and buy some more, " +
-        "99 bottles of beer on the wall.\n"
-
-      else
-        "#{number} bottles of beer on the wall, #{number} bottles of beer.\nTake one down and pass it around, #{number - 1} bottles of beer on the wall.\n"
-
-      end
-    end
-end
-
-describe Bottles do
-    let(:song) { subject }
-
-    it 'can sing a typical verse' do
-      expected = "8 bottles of beer on the wall, 8 bottles of beer.\nTake one down and pass it around, 7 bottles of beer on the wall.\n"
-      expect(song.verse(8)).to eq(expected)
-    end
-
-    it 'can sing another typical verse' do
-
-      expected = "3 bottles of beer on the wall, 3 bottles of beer.\nTake one down and pass it around, 2 bottles of beer on the wall.\n"
-      expect(song.verse(3)).to eq(expected)
-    end
-
-    it 'can sing about 1 bottle' do
-
-      expected = "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
-      expect(song.verse(1)).to eq(expected)
-    end
-
-    it 'can sing about 2 bottles' do
-
-      expected = "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n"
-      expect(song.verse(2)).to eq(expected)
-    end
-
-    it 'can sing about no more bottles' do
-
-      expected = "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
-      expect(song.verse(0)).to eq(expected)
-    end
-
-    it 'can string a few verses together' do
-
-      expected = "8 bottles of beer on the wall, 8 bottles of beer.\nTake one down and pass it around, 7 bottles of beer on the wall.\n\n7 bottles of beer on the wall, 7 bottles of beer.\nTake one down and pass it around, 6 bottles of beer on the wall.\n\n6 bottles of beer on the wall, 6 bottles of beer.\nTake one down and pass it around, 5 bottles of beer on the wall.\n\n"
-      expect(song.verses(8, 6)).to eq(expected)
-    end
-
-    it 'can sing the whole song' do
-
-      expect(song.sing).to eq(song.verses(99, 0))
     end
   end
+end
