c3a0f81ba4044695e5da140757a3bd4aa52aaff3
adminn <sridhar.patil@globacap.com>
2021-12-11 21:47:16 +0530

    Refactored the code

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -11,44 +11,42 @@ class Bottles
   end
 
   # This is a loop that will repeat anything within the braces (in this case, verse(number)) a hundred times, with number going from 99 to 0.
-  # Iterates the block, Passing decresing values
   def verses(starting, ending)
     starting.downto(ending).map { |i| verse(i) }.join("\n")
   end
 
-  def verse(number)
-    case number
-    when 6
-
+  def verse(n)
+    if n == 6
       '6 bottles of beer on the wall, ' \
-      "6 bottles of beer.\n" \
-      'Take one down and pass it around, ' \
-      "5 bottles of beer on the wall.\n\n"
-
-    when 1
-
-      '1 bottle of beer on the wall, ' \
-      "1 bottle of beer.\n" \
-      'Take it down and pass it around, ' \
-      "no more bottles of beer on the wall.\n"
-
-    when 2
-
-      '2 bottles of beer on the wall, ' \
-      "2 bottles of beer.\n" \
-      'Take one down and pass it around, ' \
-      "1 bottle of beer on the wall.\n"
-
-    when 0
-
-      'No more bottles of beer on the wall, ' \
-      "no more bottles of beer.\n" \
-      'Go to the store and buy some more, ' \
-      "99 bottles of beer on the wall.\n"
-
+        "6 bottles of beer.\n" \
+        'Take one down and pass it around, ' \
+        "5 bottles of beer on the wall.\n\n"
+    elsif plural?(n)
+      "#{bottle(n)} of beer on the wall, #{bottle(n)} of beer.\nTake one down and pass it around, #{bottle(n - 1)} of beer on the wall.\n"
+    elsif singular?(n)
+      "#{bottle(n)} of beer on the wall, #{bottle(n)} of beer.\nTake it down and pass it around, #{bottle(n - 1)} of beer on the wall.\n"
     else
-      "#{number} bottles of beer on the wall, #{number} bottles of beer.\nTake one down and pass it around, #{number - 1} bottles of beer on the wall.\n"
+      "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
+    end
+  end
 
+  private
+
+  def plural?(n)
+    n >= 2
+  end
+
+  def singular?(n)
+    n == 1
+  end
+
+  def bottle(n)
+    if plural?(n)
+      "#{n} bottles"
+    elsif singular?(n)
+      "#{n} bottle"
+    else
+      'no more bottles'
     end
   end
 end
